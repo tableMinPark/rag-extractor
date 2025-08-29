06ef5a21f0d055752be91f1ad654b4f9b8c7485c
tableminpark <tablemin_park@daum.net>
2025-08-29 11:09:10 +0900

    feat: 스웨거 적용

--- src/main/java/com/extractor/adapter/in/ExtractController.java
+++ src/main/java/com/extractor/adapter/in/ExtractController.java
@@ -7,17 +7,26 @@ import com.extractor.application.usecase.ChunkHwpxUseCase;
 import com.extractor.domain.model.HwpxDocument;
 import com.extractor.domain.vo.document.OriginalDocumentVo;
 import com.extractor.domain.vo.pattern.ChunkPatternVo;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Map;
 
-@RestController
-@RequestMapping("/extract")
+@Tag(name = "ExtractController")
 @RequiredArgsConstructor
+@RequestMapping("/extract")
+@RestController
 public class ExtractController {
 
     private final ChunkHwpxUseCase chunkHwpxUseCase;
@@ -25,10 +34,20 @@ public class ExtractController {
     @Value("${env.upload-path}")
     private String UPLOAD_PATH;
 
-    @PostMapping
+    @PostMapping(path = "/hwp", consumes =  MediaType.MULTIPART_FORM_DATA_VALUE)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success", content = {@Content(schema = @Schema(implementation = ExtractHwpxResponseDto.class))}),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = {@Content(schema = @Schema(implementation = Map.class))}),
+    })
+    @Operation(summary = "한글 문서 전처리")
     public ResponseEntity<ExtractHwpxResponseDto> extractHwpx(
-            @RequestPart("requestDto") ExtractHwpxRequestDto extractHwpxRequestDto,
-            @RequestPart("uploadFile") MultipartFile multipartFile
+            @Parameter(name = "extractHwpxRequestDto", description = "전처리 요청 정보", required = true)
+            @RequestPart("requestDto")
+            ExtractHwpxRequestDto extractHwpxRequestDto,
+
+            @Parameter(name = "uploadFile", description = "업로드 파일", required = true)
+            @RequestPart("uploadFile")
+            MultipartFile multipartFile
     ) {
         // 파일 업로드
         OriginalDocumentVo originalDocumentVo = FileUtil.uploadFile(UPLOAD_PATH, multipartFile);
@@ -37,10 +56,10 @@ public class ExtractController {
                 extractHwpxRequestDto.getPatterns(), extractHwpxRequestDto.getStopPatterns()));
 
         // 파일 삭제
-         FileUtil.deleteFile(originalDocumentVo.getFullPath());
+        FileUtil.deleteFile(originalDocumentVo.getFullPath());
 
         // 압축 해제 폴더 삭제
-         FileUtil.deleteDirectory(hwpxDocument.getUnZipPath());
+        FileUtil.deleteDirectory(hwpxDocument.getUnZipPath());
 
         return ResponseEntity.ok(ExtractHwpxResponseDto.builder()
                 .lines(hwpxDocument.getLines())
@@ -53,6 +72,6 @@ public class ExtractController {
      */
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<Map<String, ?>> handleRuntimeException(RuntimeException e) {
-        return ResponseEntity.badRequest().body(Map.of("message", e.getMessage()));
+        return ResponseEntity.internalServerError().body(Map.of("message", e.getMessage()));
     }
 }
--- src/main/java/com/extractor/adapter/in/dto/ExtractHwpxRequestDto.java
+++ src/main/java/com/extractor/adapter/in/dto/ExtractHwpxRequestDto.java
@@ -1,6 +1,7 @@
 package com.extractor.adapter.in.dto;
 
 import com.extractor.domain.vo.pattern.PatternVo;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,10 +11,13 @@ import java.util.List;
 @Getter
 @Setter
 @NoArgsConstructor
+@Schema(description = "전처리 패턴 정보")
 public class ExtractHwpxRequestDto {
 
+    @Schema(description = "전처리 패턴", example = "[{\"depth\":0,\"prefix\":\"^[\\\\[\\\\(][^\\\\]\\\\)]*별표[^\\\\]\\\\)]*[\\\\]\\\\)]\"},{\"depth\":0,\"prefix\":\"^부(\\\\s+)?칙\"},{\"depth\":0,\"prefix\":\"^제[0-9]{1,3}장[가-힣]+\"},{\"depth\":1,\"prefix\":\"^제[0-9]{1,3}절[가-힣]+\"},{\"depth\":2,\"prefix\":\"^제[0-9]{1,3}조(\\\\([가-힣]+\\\\))?\"}]")
     private List<PatternVo> patterns;
 
+    @Schema(description = "전처리 중단 패턴", example = "[{\"depth\":0,\"prefix\":\"^[\\\\[\\\\(][^\\\\]\\\\)]*(별지|별첨|서식)[^\\\\]\\\\)]*[\\\\]\\\\)]\"}]")
     private List<PatternVo> stopPatterns;
 
     public ExtractHwpxRequestDto(List<PatternVo> patterns, List<PatternVo> stopPatterns) {
