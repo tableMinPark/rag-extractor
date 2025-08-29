5e5adbc4b5e0d388ba58b522549d9a0cdd9f6290
tableminpark <tablemin_park@daum.net>
2025-08-29 11:09:10 +0900

    feat: 스웨거 적용

--- src/main/java/com/extractor/domain/vo/pattern/PatternVo.java
+++ src/main/java/com/extractor/domain/vo/pattern/PatternVo.java
@@ -1,12 +1,15 @@
 package com.extractor.domain.vo.pattern;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
 public class PatternVo {
 
+    @Schema(description = "제목 번호")
     private final int depth;
 
+    @Schema(description = "정규식")
     private final String prefix;
 
     public PatternVo(int depth, String prefix) {
