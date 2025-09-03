c816e126578b7f0058beb4750bbd7a13de3b822b
tableminpark <tablemin_park@daum.net>
2025-09-03 09:31:43 +0900

    refac: 코드 정리

--- src/main/java/com/extractor/adapter/in/dto/ExtractRequestDto.java
+++ src/main/java/com/extractor/adapter/in/dto/ExtractRequestDto.java
@@ -14,12 +14,12 @@ import java.util.List;
 @Schema(description = "전처리 패턴 정보")
 public class ExtractRequestDto {
 
-    @Schema(description = "최대 토큰 사이즈", example = "2000")
+    @Schema(description = "기본 제약 토큰 수", example = "1000")
     private Integer tokenSize;
 
-    @Schema(description = "전처리 패턴", example = "[{\"depth\":0,\"prefix\":\"^[\\\\[\\\\(][^\\\\]\\\\)]*별표[^\\\\]\\\\)]*[\\\\]\\\\)]\"},{\"depth\":0,\"prefix\":\"^부(\\\\s+)?칙\"},{\"depth\":0,\"prefix\":\"^제[0-9]{1,3}장[가-힣]+\"},{\"depth\":1,\"prefix\":\"^제[0-9]{1,3}절[가-힣]+\"},{\"depth\":2,\"prefix\":\"^제[0-9]{1,3}조(\\\\([가-힣]+\\\\))?\"}]")
+    @Schema(description = "전처리 패턴", example = "[{\"depth\":0,\"tokenSize\":1000,\"prefix\":\"^[\\\\[\\\\(][^\\\\]\\\\)]*별표[^\\\\]\\\\)]*[\\\\]\\\\)]\"},{\"depth\":0,\"tokenSize\":1000,\"prefix\":\"^부(\\\\s+)?칙\"},{\"depth\":0,\"tokenSize\":1000,\"prefix\":\"^제[0-9]{1,3}장[가-힣]+\"},{\"depth\":1,\"tokenSize\":1000,\"prefix\":\"^제[0-9]{1,3}절[가-힣]+\"},{\"depth\":2,\"tokenSize\":1000,\"prefix\":\"^제[0-9]{1,3}조(\\\\([가-힣]+\\\\))?\"}]")
     private List<PatternVo> patterns;
 
-    @Schema(description = "전처리 중단 패턴", example = "[{\"depth\":0,\"prefix\":\"^[\\\\[\\\\(][^\\\\]\\\\)]*(별지|별첨|서식)[^\\\\]\\\\)]*[\\\\]\\\\)]\"}]")
-    private List<PatternVo> stopPatterns;
+    @Schema(description = "전처리 중단 패턴", example = "[\"^[\\\\[\\\\(][^\\\\]\\\\)]*(별지|별첨|서식)[^\\\\]\\\\)]*[\\\\]\\\\)]\"]")
+    private List<String> stopPatterns;
 }
