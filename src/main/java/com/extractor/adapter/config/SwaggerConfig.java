48e426bfe85ff7fdad329da0adf32f961ba67381
tableminpark <tablemin_park@daum.net>
2025-10-20 15:02:23 +0900

    feat: 버전 변경

--- src/main/java/com/extractor/adapter/config/SwaggerConfig.java
+++ src/main/java/com/extractor/adapter/config/SwaggerConfig.java
@@ -16,6 +16,6 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("Extractor API Docs")
                         .description("문서 전처리 모듈")
-                        .version("0.0.6"));
+                        .version("0.1.0"));
     }
 }
--- src/main/java/com/extractor/adapter/in/dto/request/ExtractDocumentRequestDto.java
+++ src/main/java/com/extractor/adapter/in/dto/request/ExtractDocumentRequestDto.java
@@ -1,5 +1,6 @@
 package com.extractor.adapter.in.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,5 +12,6 @@ import lombok.Setter;
 @AllArgsConstructor
 public class ExtractDocumentRequestDto {
 
+    @Schema(description = "표 추출 타입", example = "markdown")
     private String extractType;
 }
