6ff737eee2a8df7b9bb33e6cf8149ddae87005b4
tableminpark <tablemin_park@daum.net>
2025-12-01 01:10:56 +0900

    refac: HTML MARKDOWN 변환 모듈 수정

--- src/test/java/com/extractor/global/utils/HtmlUtilTest.java
+++ src/test/java/com/extractor/global/utils/HtmlUtilTest.java
@@ -106,31 +106,58 @@ class HtmlUtilTest {
       </tr>
     </table>
     테스트 본문
-    <table>
-      <thead>
-        <tr>
-          <th>A</th>
-          <th>B</th>
-          <th>C</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>D</td>
-          <td>E</td>
-          <td>F</td>
-        </tr>
-        <tr>
-          <td>1</td>
-          <td>2</td>
-          <td>3</td>
-        </tr>
-        <tr>
-          <td>4</td>
-          <td>5</td>
-          <td>6</td>
-        </tr>
-      </tbody>
+    <table border="1">
+      <tr>
+        <th rowspan="2">A</th>
+        <th>B</th>
+        <th>C</th>
+      </tr>
+      <tr>
+        <th colspan="2">D</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td rowspan="2" colspan="2">2</td>
+      </tr>
+      <tr>
+        <td>
+            <table border="1">
+              <tr>
+                <th rowspan="2">A</th>
+                <th>B</th>
+                <th>C</th>
+              </tr>
+              <tr>
+                <th colspan="2">D</th>
+              </tr>
+              <tr>
+                <td>1</td>
+                <td rowspan="2" colspan="2">2</td>
+              </tr>
+              <tr>
+                <td>
+                    <table border="1">
+                      <tr>
+                        <th rowspan="2">A</th>
+                        <th>B</th>
+                        <th>C</th>
+                      </tr>
+                      <tr>
+                        <th colspan="2">D</th>
+                      </tr>
+                      <tr>
+                        <td>1</td>
+                        <td rowspan="2" colspan="2">2</td>
+                      </tr>
+                      <tr>
+                        <td>3</td>
+                      </tr>
+                    </table>
+                </td>
+              </tr>
+            </table>
+        </td>
+      </tr>
     </table>
     """;
 
