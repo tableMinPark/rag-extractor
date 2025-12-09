53709dd98d66748b54ca1ce424603e6c4cadd105
tableminpark <tablemin_park@daum.net>
2025-12-10 02:40:43 +0900

    fix: 변수 수정

--- src/main/java/com/document/extractor/application/port/DocumentReadPort.java
+++ src/main/java/com/document/extractor/application/port/DocumentReadPort.java
@@ -16,7 +16,9 @@ public interface DocumentReadPort {
     /**
      * 원격 문서 조회
      *
-     * @param url 원격 문서 URL
+     * @param repoType        원격 문서 타입
+     * @param uri             원격 문서 URI
+     * @param extractTypeCode 표 추출 타입 코드
      */
-    Document getRepoDocumentPort(String url);
+    Document getRepoDocumentByUriPort(String repoType, String uri, String extractTypeCode);
 }
